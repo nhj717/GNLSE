ec26674f1e76c73758b6a20bbc997451e68de282
nhj717 <nhj717@homtail.com>
2025-10-28 15:10:20 +0100

    propagation over P complete

--- gnlse/GNLSE_over_P.py
+++ gnlse/GNLSE_over_P.py
@@ -31,16 +31,13 @@ T0 = T_FWHM / (2 * sqrt(log(2)))
 # R_R = 80e6
 # P0 = Pavg / (T0 * R_R)
 Np = 2**7
-P_max = 10e3
+P_max = 30e3
 delP = P_max / Np
 P0 = arange(1, Np + 1) * delP
 
 # Fiber informaiton
 omega = data[data_label.index("omega")]
 beta1 = data[data_label.index("beta1")]
-beta2 = data[data_label.index("beta2")]
-beta3 = data[data_label.index("beta3")]
-beta4 = data[data_label.index("beta4")]
 neff = data[data_label.index("n_eff")]
 n = real(neff)
 k = -imag(neff)
@@ -49,7 +46,7 @@ beta0 = n * omega / c
 beta0_spl = UnivariateSpline(omega, beta0, k=5)
 beta1_spl = UnivariateSpline(omega, beta1, k=5)
 alpha_spl = UnivariateSpline(omega, alpha, k=5)
-n2 = 2.6e-20  # nonlinear index in m^2/W
+n2 = 2.7e-20  # nonlinear index in m^2/W
 A_eff = 9.2e-12  # Effective mode area in m2
 gamma = 2 * pi * n2 / lambda0 / A_eff
 # gamma = 0
